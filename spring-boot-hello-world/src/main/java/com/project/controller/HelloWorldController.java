b41b497f6564e1d38dd94bf609e15e9094100542
Saumya Awasthi <55647842+Saum112@users.noreply.github.com>
2022-10-09 23:59:48 +0530

    Add files via upload

---
+++ spring-boot-hello-world/src/main/java/com/project/controller/HelloWorldController.java
@@ -0,0 +1,35 @@
+package com.project.controller;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("/hello")
+	public Map<String, String> sayHello() {
+		HashMap<String, String> map = new HashMap<>();
+		map.put("message", "hello world");
+		return map;
+	}
+
+	@PostMapping(produces = { "application/json" }, value = { "/hello" })
+	public Map<String, String> postHello(@RequestBody Map<String, String> data) {
+		HashMap<String, String> map = new HashMap<>();
+		String li = data.values().stream().toList().get(0);
+		String val = "hello " + li;
+		map.put("message", val);
+		return map;
+	}
+
+}
